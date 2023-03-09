6e712335f1fe49f99c05844d41abf27bed539480
Kacper Lewko <k.lewkosga@gmail.com>
2023-03-09 13:24:37 +0100

    addidg new task and showing if a task is done

--- js/script.js
+++ js/script.js
@@ -20,21 +20,41 @@
 
         for (const task of tasks) {
             htmlString += `
-                <li>
+                <li ${task.done ? " class=\"task__decoration\"" : ""}>
                     ${task.content}
                 </li>
             `;
         }
         
         document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
+
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        })
+        render();
     }
 
+const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newTaskContent = document.querySelector(".js-newTask").value.trim();
+    if (newTaskContent === "") {
+        return;
+    }
+
+    addNewTask(newTaskContent);
+}
 
     const init = () => {
         welcome();
         render();
-    };
+    
+        const form = document.querySelector(".js-form");
 
+        form.addEventListener("submit", onFormSubmit);
+}
 
     init();
-};
+}
